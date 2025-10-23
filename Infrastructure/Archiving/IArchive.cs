a2ae7f886dd83fe31a200d05c89585f8d6a5ef8d
Azarian Sayed Danial Amirovich <danialazarian@gmail.com>
2025-10-24 01:33:58 +0300

    Delete Infrastructure directory

--- Infrastructure/Archiving/IArchive.cs
+++
@@ -1,9 +0,0 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab2.Domain.Messages;
-using Itmo.ObjectOrientedProgramming.Lab2.Domain.Results;
-
-namespace Itmo.ObjectOrientedProgramming.Lab2.Infrastructure.Archiving;
-
-public interface IArchive
-{
-    ArchiveResult Save(Message message);
-}
--- Infrastructure/Notifications/INotifier.cs
+++
@@ -1,8 +0,0 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab2.Domain.Results;
-
-namespace Itmo.ObjectOrientedProgramming.Lab2.Infrastructure.Notifications;
-
-public interface INotifier
-{
-    NotifyResult Notify();
-}
